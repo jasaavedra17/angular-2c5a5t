8b84e5c9140911e6682b5457f1a80721c648cfc1
jasaavedra17 <jasaavedra17@gmail.com>
2019-12-19 22:09:15 +1100

    Initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+
+import { AppComponent } from './app.component';
+import { HelloComponent } from './hello.component';
+import { ProductComponent } from './components/product/product.component';
+import { ClientComponent } from './components/client/client.component';
+import { FooterComponent } from './components/layout/footer/footer.component';
+import { HeaderComponent } from './components/layout/header/header.component';
+import { TestComponent } from './components/layout/test/test.component';
+
+import { FontAwesomeModule } from './fontawesome/angular-fontawesome';
+
+@NgModule({
+  imports:      [ BrowserModule, FormsModule, FontAwesomeModule ],
+  declarations: [ AppComponent, HelloComponent, ProductComponent, ClientComponent, FooterComponent, HeaderComponent, TestComponent ],
+  bootstrap:    [ AppComponent ]
+})
+export class AppModule { }
